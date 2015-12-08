b115c67e673b2ecbe6e836191093eccaabba77f2
Jim Dowling <jdowling@kth.se>
2015-12-09 00:03:11 +0100

    no maven or git reqd

--- recipes/install.rb
+++ recipes/install.rb
@@ -17,13 +17,13 @@ private_ip = my_private_ip()
 public_ip = my_public_ip()
 
 
-package "git" do 
-  action :install
-end
+# package "git" do 
+#   action :install
+# end
 
-package "maven" do 
-  action :install
-end
+# package "maven" do 
+#   action :install
+# end
 
 
 # Pre-Experiment Code

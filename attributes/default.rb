48626cd23933ec0485ef37ea7e39b86a94d770a3
Ashansa Perera <ashansa.perera@gmail.com>
2016-04-22 10:40:30 +0200

    reducing default load

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,15 @@
 # 'user' and 'group' define the unix user and group, respectively, 
 # that the experiment will be excecuted as.
 default[:streamingbenchmarks][:group] = "hadoop"
-default[:streamingbenchmarks][:numCustomers] = "1000"
-default[:streamingbenchmarks][:numStores] = "1000"
+default[:streamingbenchmarks][:numCustomers] = "100"
+default[:streamingbenchmarks][:numStores] = "100"
 default[:streamingbenchmarks][:user] = "flink"
 
 default[:streamingbenchmarks][:dir] = "/srv" 
 default[:streamingbenchmarks][:home] = node[:streamingbenchmarks][:dir] + "/intel_streamingbench" 
-default[:streamingbenchmarks][:numABRecords] = "10000000" 
+default[:streamingbenchmarks][:numABRecords] = "10000" 
 default[:streamingbenchmarks][:numABGroups] = "5"
-default[:streamingbenchmarks][:numTweets] = "25000000"
+default[:streamingbenchmarks][:numTweets] = "25000"
 
 default[:streamingbenchmarks][:replicasTweets]   = "1"
 default[:streamingbenchmarks][:partitionsTweets] = "1"
